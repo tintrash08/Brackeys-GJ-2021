1c66a7d1e859720da55a418f22781e3dc5d3338f
Nishant Khalate <nishantkhalate10@gmail.com>
2021-02-18 01:32:46 +0530

    drone enemy

---
+++ Assets/Scripts/Enemy_Drone.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Drone : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 1.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+    }
+}
